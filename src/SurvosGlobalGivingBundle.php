11af89303098fb36e521fe9cb61fda6c6702c70a
tacman <info@survos-ci.com>
2024-02-04 11:22:48 +0000

    add option to get by key

---
+++ src/SurvosGlobalGivingBundle.php
@@ -0,0 +1,50 @@
+<?php
+
+/** generated from /home/tac/g/survos/survos/packages/maker-bundle/templates/skeleton/bundle/src/Bundle.tpl.php */
+
+namespace Survos\GlobalGivingBundle;
+
+use Survos\BarcodeBundle\Service\BarcodeService;
+use Survos\GlobalGivingBundle\Service\GlobalGivingService;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class SurvosGlobalGivingBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+
+//        $serviceId = 'survos_barcode.barcode_service';
+//        $container->services()->alias(BarcodeService::class, $serviceId);
+        $builder->autowire(GlobalGivingService::class)
+            ->setPublic(true)
+            ->setArgument('$httpClient', new Reference('http_client'))
+            ->setArgument('$apiKey', $config['api_key'])
+
+        ;
+        // $builder->setParameter('survos_workflow.direction', $config['direction']);
+
+        // twig classes
+
+        /*
+        $definition = $builder
+        ->autowire('survos.barcode_twig', BarcodeTwigExtension::class)
+        ->addTag('twig.extension');
+
+        $definition->setArgument('$widthFactor', $config['widthFactor']);
+        $definition->setArgument('$height', $config['height']);
+        $definition->setArgument('$foregroundColor', $config['foregroundColor']);
+        */
+    }
+
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+            ->scalarNode('api_key')->defaultValue('')->end()
+            ->end();
+    }
+}
